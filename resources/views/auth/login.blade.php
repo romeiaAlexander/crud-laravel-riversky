9795ba130a6362491edfa7aaf3d27c3c8b1c8c5c
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-06-24 21:37:26 +0700

    make navbar, add assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,28 @@
-@extends('include.layout')
 @section('title', 'Login')
+@extends('include.layout')
 @section('content')
-    <div class="container p-5">
-      <form class="ms-auto me-auto" style="width: 500px">
-        <div class="mb-3">
-          <label for="exampleInputUsername" class="form-label">Username</label>
-          <input type="text" class="form-control" id="exampleInputUsername">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword">
-        </div>
-        <div class="mb-3">
-          <a href="#">Apakah anda sudah mempunyai akun?</a>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+  <link rel="stylesheet" href="{{ asset('assets/css/style2.css') }}">
+    <div class="container">
+      <div class="box">
+          <div class="header">
+              <header><img src="{{ asset('/assets/images/logo4.png') }}" alt="logo"></header>
+              <p>Welcome to Riversky</p>
+          </div>
+          <form action="" method="post">
+            <div class="input-box">
+                <label for="Username">Username</label>
+                <input type="text" class="input-field" name="username" id="username" required>
+                <i class="bx bx-envelope"></i>
+            </div>
+            <div class="input-box">
+                <label for="pass">Password</label>
+                <input type="password" class="input-field" name="password" id="password" required>
+                <i class="bx bx-lock"></i>
+            </div>
+            <div class="input-box">
+                <input type="submit" name="submit" class="input-submit" value="Login">
+            </div>
+          </form>
+      </div>
     </div>
 @endsection
--- resources/views/include/navigasi.blade.php
+++ resources/views/include/navigasi.blade.php
@@ -1,36 +1,88 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+<div class="sub_page">
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="#">
+            <span>
+              21Versky
+            </span>
+          </a>
+          
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="s-1"> </span>
+            <span class="s-2"> </span>
+            <span class="s-3"> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#"> Registerasi </a>
+                </li>
+              </ul>
+            </div>
+            <div class="quote_btn-container ">
+              <a href="">
+                <img src="{{ asset('assets/images/call.png') }}" alt="">
+                <span>
+                  Call : + 62 815-2619-0671
+                </span>
+              </a>
+              <form class="form-inline">
+                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+              </form>
+            </div>
+          </div>
+        </nav>
       </div>
-    </div>
-  </nav>
+    </header>
+    <!-- end header section -->
+
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6 ">
+                  <div class="detail_box">
+                    <h1>
+                      Kunjungi Kami
+                    </h1>
+                    <p>
+                      Program Studi Sistem Informasi Jl. Fakultas MIPA UNSRAT,<br>
+                      Kleak, Kec. Malalayang, Kota Manado, Prov. Sulawesi Utara<br>
+                      Kode Pos 95115
+                    </p>
+                    <div class="btn-box">
+                      <a href="" class="btn-1">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="img-box">
+                    <img src="assets/images/gambar3.png" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel_btn-container">
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+</div>
